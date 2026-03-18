6738fc3379880286e9125c07e0c65f74dfe3a89d
frozzzq <veggjoss@gmail.com>
2026-03-18 08:59:56 +0000

    mejora de script update-byle.sh

--- scripts/update-byle.sh
+++ scripts/update-byle.sh
@@ -1,10 +1,10 @@
 echo "haciendo pull..."
-cd ~/ByleAdministration_repositorio/ByleAdministration
+cd /home/frozz/ByleAdministration_repositorio/ByleAdministration
 git pull
 
 echo "reiniciando app..."
 
-pm2 restart byle-web
+su -c "pm2 restart byle-web" frozz
 cloudflared tunnel run --url http://localhost:3000 byle-tunnel
 
 echo "listo!"
