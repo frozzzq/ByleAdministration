d74b6597731cd2476be091b3b3342d6f7c9631c2
frozzzq <veggjoss@gmail.com>
2026-03-18 07:23:38 +0000

    creacion y hosteo de pagina web, (pagina en blanco para pruebas), creacion de carpeta de scripts

---
+++ web/byle-web/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('<h1>byle.site funcionando</h1/>');
+});
+
+app.listen(3000, () => {
+	console.log('servidor corriendo en puerto 3000');
+});
