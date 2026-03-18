a954b14bd7e3574605db6be6486cbdabbd3a8397
frozz <veggjoss@gmail.com>
2026-03-18 12:10:22 -0600

    shhhhhhh

--- web/byle-web/src/index.js
+++ web/byle-web/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('<h1>byle.site funcionando</h1/>');
+	res.send('<h1>KAZZURA JOTO</h1/>');
 });
 
 app.listen(3000, () => {
