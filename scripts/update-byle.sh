8ccbf601bd4c1312e782f1370c213f88c53e743f
frozz <veggjoss@gmail.com>
2026-03-18 02:07:53 -0600

    nueva linea para inicio de servicio cloudflare

--- scripts/update-byle.sh
+++ scripts/update-byle.sh
@@ -3,6 +3,8 @@ cd ~/ByleAdministration_repositorio/ByleAdministration
 git pull
 
 echo "reiniciando app..."
+
 pm2 restart byle-web
+cloudflared tunnel run --url http://localhost:3000 byle-tunnel
 
 echo "listo!"
